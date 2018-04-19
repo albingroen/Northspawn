ba4eff5da134072376f67312ace7ddf9447b53a3
Albin Groen <albin.groen@gmail.com>
2018-04-19 10:17:33 +0200

    Added header to landing page.

---
+++ incs/header/header.php
@@ -0,0 +1,9 @@
+<div class="header">
+  <h3>NorthSpawn 2018</h3>
+  <h1>E-sport tävlingar. Lan event. Hackathons.</h1>
+  <button>Anmäl intresse</button>
+</div>
+<style>
+  <?php include('header.css') ?>
+</style>
+
---
+++ landing.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="styles/main.css">
+  <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+  <?php require('incs/header/header.php') ?>
+  <h1>content</h1>
+  <?php require('incs/footer/footer.php') ?>
+</body>
+</html>
