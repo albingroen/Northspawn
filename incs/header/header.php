07c195b0249b33e5b90799ad375f857449483e98
Albin Groen <albin.groen@gmail.com>
2018-05-04 12:00:10 +0200

    Style changes in header

--- incs/header/header.php
+++ incs/header/header.php
@@ -1,6 +1,8 @@
-<!-- <script>
-  document.getElementByClassName("title").innerHTML = "lan";
-</script> -->
+
+
+
+  
+
 <nav>
   <ul>
     <li><a href="#">Start</a></li>
@@ -17,9 +19,17 @@
     
   </div>
   <!-- <h1 class="title" >E-sport tävlingar. Lan event. Cosplay. Hackathons.</h1> -->
-  <h1>GAMING</h1>
-  <h1>GAMING</h1>
-  <h1>GAMING</h1>
+  <h1 id="title" >HACKATHON</h1>
+  <h1 id="title2" >HACKATHON</h1>
+  <h1 id="title3" >HACKATHON</h1>
+  <!-- <?php 
+
+    for($i = 0; $i<3; $i++) {
+      echo <<<TITLE
+      <h1>Hackathon</h1>
+TITLE;
+    }
+  ?> -->
   <h2>4 dagar fullspäckade med elektronik</h2>
   <div class="button-wrapper">
     <a href="register.php">
@@ -33,4 +43,38 @@
 <style>
   <?php include('header.css') ?>
 </style>
+<script type='text/javascript'>  
+  window.onload = function() {
+  
+    
+    setTimeout(() => {
+      document.getElementById("title").innerHTML = "hackathon";
+      document.getElementById("title2").innerHTML = "hackathon";
+      document.getElementById("title3").innerHTML = "hackathon"; 
+    }, 0);
+
+    setTimeout(() => {
+      document.getElementById("title").innerHTML = "gaming";
+      document.getElementById("title2").innerHTML = "gaming";
+      document.getElementById("title3").innerHTML = "gaming"; 
+    }, 3000);
+
+    setTimeout(() => {
+      document.getElementById("title").innerHTML = "expo";
+      document.getElementById("title2").innerHTML = "expo";
+      document.getElementById("title3").innerHTML = "expo";
+    }, 6000);
+
+    setTimeout(() => {
+      document.getElementById("title").innerHTML = "esport";
+      document.getElementById("title2").innerHTML = "esport";
+      document.getElementById("title3").innerHTML = "esport";
+    }, 9000);
+      
+    
+    
+
+  }
+</script>
+
 
